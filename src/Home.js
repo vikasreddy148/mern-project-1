29c52bb42f8abbdb23ea2a8069e6e67daa60b788
vikasreddy148 <kathulavikasr@gmail.com>
2025-06-18 20:06:35 +0530

    Setup + Bootstrap Integration + Routing Setup

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="container text-center">
+      <h1>Welcome to MERN Projects</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="container text-center">
+      <h1>Login Page</h1>
+    </div>
+  );
+}
+
+export default Login;
